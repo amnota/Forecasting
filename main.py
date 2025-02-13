7b46633b577ffc319a191b166ba60b0aa7207a44
June <junesamejs@gmail.com>
2025-02-13 09:18:01 +0700

    Use PORT environment variable for deployment

--- main.py
+++ main.py
@@ -80,5 +80,7 @@ def get_demand_comparison():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))  # ดึงค่าจาก Environment Variable
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
